5c35364898ca3be7fecefe0521e10bfa0174bd3c
Kristina Jovanovska <72104951+kristinca@users.noreply.github.com>
2022-01-01 18:09:25 +0100

    Commit changes

--- config/settings.py
+++ config/settings.py
@@ -16,15 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%jh0-y*27ahc4dh^f!63nfw6&s9(#00b#yl4l$zmq7lra_id1%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
